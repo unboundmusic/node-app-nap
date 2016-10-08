1aa3ef3ef2896af7a2c48ad8dfe8a01a23d5d4e3
Ben Meyer <unboundmusic@me.com>
2016-10-07 18:41:05 -0700

    added index

---
+++ index.js
@@ -0,0 +1,15 @@
+const addon = require('bindings')('AppNap.node')
+
+module.exports = {
+	disable: function (reason = '') {
+	  if (process.platform !== 'darwin') {
+	    throw new Error('System is not macOS')
+	  }
+
+	  if (!reason || reason.length === 0 || typeof reason !== 'string') {
+	    throw new Error('Argument of type string expected')
+	  }
+
+	  return addon.disable(reason)
+	}
+}
